0b44e141b9a088dfd03159183eaaea4332ebc7ff
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-08-21 17:33:37 +0800

    Level-0 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,8 +6,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        String horizontal = "____________________________________________________________";
+        System.out.println(horizontal);
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println(horizontal);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal);
     }
 }
