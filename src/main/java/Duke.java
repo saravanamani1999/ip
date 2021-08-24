593cc2111d6d986425876203b9b22400fa7e7455
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-08-24 18:08:09 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 public class Duke {
         public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "      ____        _        \n"
+                + "     |  _ \\ _   _| | _____ \n"
+                + "     | | | | | | | |/ / _ \\\n"
+                + "     | |_| | |_| |   <  __/\n"
+                + "     |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("     Hello from\n" + logo);
         String horizontal = "    ____________________________________________________________";
         System.out.println(horizontal);
         System.out.println("     Hello! I'm Duke\n" + "     What can I do for you?");
         System.out.println(horizontal);
         String line;
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int listCount = 0;
         while (true){
             System.out.println("");
             line = in.nextLine();
@@ -22,9 +24,21 @@ public class Duke {
                 System.out.println(horizontal);
                 break;  
             }
-            else{
+            else if (line.equals("list")){
                 System.out.println(horizontal);
-                System.out.println("     "+line);
+                for (int x = 0; x < list.length; x += 1){
+                    if(list[x] != null){
+                        System.out.println("     " +(x+1)+ ". " + list[x]);
+                    }
+                }
+                System.out.println(horizontal);
+
+            }
+            else {
+                System.out.println(horizontal);
+                System.out.println("     added: "+line);
+                list[listCount] = line;
+                listCount += 1;
                 System.out.println(horizontal);
             }
             
