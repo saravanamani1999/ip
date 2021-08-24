b00f170081b54fda7ff209221762ecf4157f9378
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-08-24 22:10:27 +0800

    Level-3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,15 +12,11 @@ public class Duke {
         String horizontal = "    ____________________________________________________________";
         System.out.println(horizontal);
         System.out.println("     Hello! I'm Duke\n" + "     What can I do for you?");
-        
         System.out.println(horizontal);
         String userInput;
-        String[] list = new String[100];
-        int listCount = 0;
         Scanner in = new Scanner(System.in);
         boolean userExit = false;
-        
-        
+        ArrayList<Task> list = new ArrayList<>();
         
         while (!userExit){
 
@@ -36,28 +32,44 @@ public class Duke {
             } else if (userInput.equals("list")){
                 System.out.println(horizontal);
 
-                if(list[0] == null){
+                if(list.size() == 0){
                     System.out.println("     No tasks added.");
                 }
-                for (int x = 0; x < list.length; x += 1){
-                    if(list[x] != null){
-                        System.out.println("     "+ (x+1) + ". " + list[x]);
-                    }
+                for (int x = 0; x < list.size(); x += 1){
+                    System.out.println("     "+ (x+1) + "." + "[" 
+                                        + list.get(x).getStatusIcon() + "] " 
+                                        + list.get(x).description);
+                    
                 }
                 System.out.println(horizontal);
+            }
+
+            else if (userInput.length() > 3 && userInput.substring(0, 4).equals("done")){
+
+                String digit = userInput.substring(4, userInput.length()).trim();
+                int taskNumber = Integer.parseInt(digit) - 1;
+                System.out.println(horizontal);
+                if (list.get(taskNumber).isDone){
+                    System.out.println("     Task is already marked as done.");
+                } else{
+                    list.get(taskNumber).markAsDone();
+                    System.out.println("     Nice! I've marked this task as done:");
+                    System.out.println("       [" + list.get(taskNumber).getStatusIcon() + "] " 
+                                        + list.get(taskNumber).description);
+                }
+
+                System.out.println(horizontal);
+
 
             } else {
                 System.out.println(horizontal);
                 System.out.println("     added: " + userInput);
-                list[listCount] = userInput;
-                listCount += 1;
+                list.add(new Task(userInput));
                 System.out.println(horizontal);
-            }
-            
-            
+            }    
         }
 
         in.close();
-   
     }
 }
+
