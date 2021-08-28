6f6a81120eee4756f918a0348f05d1fbc3f843c3
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-08-29 02:41:31 +0800

    move methods to Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,33 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    private static Task[] tasks = new Task[100];
+    private static String horizontalLine = "    ____________________________________________________________";
+    private static int taskCount = 0;
+    private static boolean hasUserExited = false;
+
+    public static void addTask(Task t) {
+        tasks[taskCount] = t;
+        System.out.println(t.toPrint());
+        taskCount++;
+    }
+
+    public static void listOfTasks() {
+        System.out.println(horizontalLine);
+        for (Task t : tasks) {
+            if (t != null) {
+                System.out.println(t.list());
+            }
+        }
+        System.out.println(horizontalLine);
+    }
+
+    public static void exit() {
+        System.out.println(horizontalLine + "\n     Bye. Hope to see you again soon!\n" + horizontalLine);
+        hasUserExited = true;
+    }
+
+
     public static void main(String[] args) {
         String logo = "      ____        _        \n"
                 + "     |  _ \\ _   _| | _____ \n"
@@ -9,73 +36,29 @@ public class Duke {
                 + "     | |_| | |_| |   <  __/\n"
                 + "     |____/ \\__,_|_|\\_\\___|\n";
         System.out.println("     Hello from\n" + logo);
-        String horizontalLine = "    ____________________________________________________________";
-        System.out.println(horizontalLine + "\n" + "     Hello! I'm Duke\n" 
-                           + "     What can I do for you?\n" + horizontalLine);
-        String userInput;
+        System.out.println(horizontalLine + "\n" + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n" + horizontalLine);
+        String getUserInput;
         Scanner in = new Scanner(System.in);
-        boolean userExited = false;
-        ArrayList<Task> tasks = new ArrayList<>();
-        
-        while (!userExited) {
+
+
+        while (!hasUserExited) {
             System.out.println("");
-            userInput = in.nextLine();
-            if (userInput.equals("bye")) {   
-                System.out.println(horizontalLine);
+            getUserInput = in.nextLine();
+            if (getUserInput.equals("bye")) {
                 exit();
-                System.out.println(horizontalLine);
-                userExited = true; 
-            } else if (userInput.equals("list")) {
-                System.out.println(horizontalLine);
-                listOfTasks(tasks);
-                System.out.println(horizontalLine);
-            } else if (userInput.length() > 3 && userInput.substring(0, 4).equals("done")) {
-                System.out.println(horizontalLine);
-                markedAsDone(tasks, userInput);
-                System.out.println(horizontalLine);
+            } else if (getUserInput.length() > 3 && getUserInput.substring(0, 4).equals("done")) {
+                int taskNumber = Integer.parseInt(getUserInput.substring(4, getUserInput.length()).trim()) - 1;
+                tasks[taskNumber].markAsDone();
+                System.out.println(tasks[taskNumber].printDone());
+            } else if (getUserInput.equals("list")) {
+                    listOfTasks();
             } else {
-                System.out.println(horizontalLine);
-                addToList(tasks, userInput);
-                System.out.println(horizontalLine);
-            }    
-        }
-    }
-
-    public static void listOfTasks(ArrayList<Task> tasks) {
-        if (tasks.size() == 0) {
-            System.out.println("     No tasks added.");
-        }
-
-        for (int i = 0; i < tasks.size(); i += 1) {
-            System.out.println("     " + (i + 1) + "." + "[" + tasks.get(i).getStatusIcon() 
-                               + "] " + tasks.get(i).description);  
-        }
-    }
-
-    public static void exit() {
-        System.out.println("     Bye. Hope to see you again soon!");  
-    }
-
-    public static void markedAsDone(ArrayList<Task> tasks, String userInput) {
-        String digit = userInput.substring(4, userInput.length()).trim();
-        int taskNumber = Integer.parseInt(digit) - 1;
-        if (tasks.get(taskNumber).isDone) {
-            System.out.println("     Task is already marked as done.");
-        } else {
-            tasks.get(taskNumber).markAsDone();
-            System.out.println("     Nice! I've marked this task as done:");
-            System.out.println("       [" + tasks.get(taskNumber).getStatusIcon() + "] " 
-                               + tasks.get(taskNumber).description);
-        }
-    }
-
-    public static void addToList(ArrayList<Task> tasks, String userInput){
-        if (userInput.length() > 0) {  
-            System.out.println("     added: " + userInput);
-            tasks.add(new Task(userInput));
-        } else {
-            System.out.println("     Please type in a task.");
+                addTask(new Task(getUserInput, taskCount));
+            }
         }
     }
 }
+
+
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,12 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String horizontalLine = "    ____________________________________________________________";
+    protected int taskCount;
 
-    public Task(String description) {
+    public Task(String description, int taskCount) {
         this.description = description;
+        this.taskCount = taskCount + 1;
         this.isDone = false;
     }
 
@@ -11,11 +14,23 @@ public class Task {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    public void markAsDone(){
+    public void markAsDone() {
         this.isDone = true;
     }
     
-    public boolean isDone(){
+    public boolean isDone() {
         return isDone;
+    }
+
+    public String list() {
+        return "     " + taskCount + "." + "[" + getStatusIcon() + "] " + description;
+    }
+
+    public String toPrint() {
+        return horizontalLine + System.lineSeparator() + "     added: " + description + System.lineSeparator() + horizontalLine;
+    }
+
+    public String printDone() {
+        return horizontalLine + System.lineSeparator() + "     Nice! I've marked this task as done:" + System.lineSeparator() + "       [" + getStatusIcon() + "] " + description + System.lineSeparator() + horizontalLine;
     }
 }
