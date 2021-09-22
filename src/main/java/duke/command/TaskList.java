ece3fee2786c38847c9467cf5518bec156327e02
saravanamani1999 <66838856+saravanamani1999@users.noreply.github.com>
2021-09-22 16:08:51 +0800

    Level-9 implemented simple find of exact terms

--- src/main/java/duke/command/TaskList.java
+++ src/main/java/duke/command/TaskList.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 public class TaskList {
 
     public ArrayList<Task> tasks = new ArrayList<>();
+    public static ArrayList<Task> matches = new ArrayList<Task>();
     protected static int taskCount = 0;
     protected static String[] description;
     protected static String[] separate;
@@ -26,6 +27,7 @@ public class TaskList {
     public static final String EVENT = "event";
     public static final String DONE = "done";
     public static final String DELETE = "delete";
+    public static final String FIND = "find";
 
     public void addTask(Task newTask) {
         tasks.add(newTask);
@@ -53,7 +55,7 @@ public class TaskList {
         Ui.printLineBottom();
     }
 
-    public  void deleteTask(int taskNumber) {
+    public void deleteTask(int taskNumber) {
         taskCount = tasks.size() - 1;
         isPlural = tasks.size() != 2;
         System.out.println(" " + tasks.get(taskNumber).printDelete() + "\nNow you have "
@@ -62,6 +64,15 @@ public class TaskList {
         Ui.printLineBottom();
         tasks.remove(taskNumber);
         Storage.saveToFile(Duke.file);
+    }
+
+    public static void getMatches(String search, ArrayList<Task> list) {
+        matches.clear();
+        for(Task task: list) {
+            if (task.description.contains(search)) {
+                matches.add(task);
+            }
+        }
     }
 
     public void executeCommand(String getUserInput, String userCommand) throws DukeException, IOException {
@@ -133,6 +144,24 @@ public class TaskList {
             } else {
                 deleteTask(taskNumber);
             }
+            break;
+        case FIND:
+            description = getUserInput.split("find ");
+            if (description[1].trim().isEmpty()) {
+                throw new IndexOutOfBoundsException();
+            }
+            getMatches(description[1],tasks);
+            int taskId = 0;
+            Ui.printLineTop();
+            Ui.matchesListHeader();
+            if (matches.size() == 0) {
+                Ui.noMatchesMessage();
+            }
+            for (int i = 0; i < matches.size(); i++) {
+                taskId = i + 1;
+                System.out.println(" " + taskId + matches.get(i).printTask());
+            }
+            Ui.printLineBottom();
             break;
         default:
             throw new InvalidCommandException();
